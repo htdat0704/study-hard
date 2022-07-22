f4f59ac134eef4f8fd28dc6bf75b7af13dc7dae1
htdat0704 <102528215+htdat0704@users.noreply.github.com>
2022-07-22 17:55:11 +0700

    Delete Blog_BK directory

--- Blog_BK/src/routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const route = express.Router();
-
-const HomeController = require('../app/controllers/HomeController');
-
-route.get('/', HomeController.index);
-
-module.exports = route;
--- Blog_BK/src/util/mongoose.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mutipleMongooseToObject: (mongooseArray) => {
-        return mongooseArray.map((moogose) => moogose.toObject());
-    },
-
-    mongooseToObject: (moongose) => {
-        return moongose ? moongose.toObject : moongose;
-    },
-};
